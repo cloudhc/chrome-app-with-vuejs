2e8604d4cf33466655a6cf592a25b43b9c1e3cb2
bast <cloudhc@naver.com>
2020-04-13 12:28:34 +0900

    apply code style.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import App from './App'
+import 'chart.js'
+import 'hchs-vue-charts'
 
-
-
-
+Vue.use(window.VueCharts)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  
-  
   render: h => h(App)
 })
